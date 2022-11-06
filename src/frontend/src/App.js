3b5ccfb5b943d61a71337a62238ac6a992232891
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-11-06 14:06:16 +0100

    no helmet warnings

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import UserPanel from './components/UserPanel';
-import { Helmet } from "react-helmet";
 
 function App() {
 
   return (
     <div className="App">
-      <Helmet>
         <title>Diet Planner - Project</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
-      </Helmet>
       <header className="App-header">
         <div className="container">
           <h1>Diet Planner</h1>
