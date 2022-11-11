1a454ffb8014586b4b5017cb802cf552be527f5e
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-11-11 21:11:55 +0100

    changes

--- src/main/java/com/example/dietplanner/user/rest/AuthController.java
+++ src/main/java/com/example/dietplanner/user/rest/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -52,7 +53,7 @@ public class AuthController {
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
         List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
+                .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(new JwtResponse(jwt,
@@ -91,20 +92,21 @@ public class AuthController {
         } else {
             for (String role : strRoles) {
                 switch (role) {
-                    case "admin":
+                    case "admin" -> {
                         Role adminRole = roleRepository.findByName(EnumRole.ROLE_ADMIN)
                                 .orElseThrow(() -> new RuntimeException("Error: role not found."));
                         roles.add(adminRole);
-                        break;
-                    case "mod":
+                    }
+                    case "mod" -> {
                         Role modRole = roleRepository.findByName(EnumRole.ROLE_MODERATOR)
                                 .orElseThrow(() -> new RuntimeException("Error: role not found."));
                         roles.add(modRole);
-                        break;
-                    default:
+                    }
+                    default -> {
                         Role userRole = roleRepository.findByName(EnumRole.ROLE_USER)
                                 .orElseThrow(() -> new RuntimeException("Error: role not found."));
                         roles.add(userRole);
+                    }
                 }
             }
         }
