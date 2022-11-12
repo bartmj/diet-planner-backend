ca8466b73ecac59b8134bac2ed7cba2eca8e5ae9
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-11-12 19:46:33 +0100

    UserDetails methods to true

--- src/main/java/com/example/dietplanner/user/security/services/UserDetailsImpl.java
+++ src/main/java/com/example/dietplanner/user/security/services/UserDetailsImpl.java
@@ -56,34 +56,22 @@ public class UserDetailsImpl implements UserDetails {
     }
 
     @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
     public boolean isAccountNonExpired() {
         return false;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
-
-
 }
