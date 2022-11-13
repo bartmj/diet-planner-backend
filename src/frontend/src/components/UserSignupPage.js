d7dda7310283d47abb728f072d4ce412c26c3213
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-11-13 17:32:18 +0100

    some html

--- src/frontend/src/components/UserSignupPage.js
+++ src/frontend/src/components/UserSignupPage.js
@@ -1,8 +1,4 @@
-import Input from './Input';
-import ButtonWithProgress from './ButtonWithProgress';
-import { useState } from "react";
-import { connect } from 'react-redux';
-import * as authActions from '../redux/authActions';
+import React, { useState } from "react";
 
 const UserSignupPage = (props) => {
     const [form, setForm] = useState({
@@ -16,99 +12,47 @@ const UserSignupPage = (props) => {
             username: form.username,
             password: form.password
         };
-        // setPendingApiCall(true);
 
-        //  TODO:
-        props.actions.postSignup(user)
-            // .then((response) => {
-            //     props.history.push('/');
-            // })
-            // .catch((apiError) => {
-            //     if (apiError.response.data && apiError.response.data.validationErrors) {
-            //         setErrors(apiError.response.data.validationErrors);
-            //     }
-            //     setPendingApiCall(false);
-            // });
     };
 
     return (
         <div className="container">
             <h1 className="text-center">Sign Up</h1>
             <div className="col-12 mb-3">
-                <Input
-                    name="displayName"
-                    label="Display Name"
-                    placeholder="Your display name"
-                    // value={form.displayName}
-                    // onChange={onChange}
-                    // hasError={errors.displayName && true}
-                    // error={errors.displayName}
-                />
             </div>
             <div className="col-12 mb-3">
-                <Input
+                <label>username</label>
+                <input
                     name="username"
-                    label="Username"
                     placeholder="Your username"
-                    // value={form.username}
-                    // onChange={onChange}
-                    // hasError={errors.username && true}
-                    // error={errors.username}
                 />
             </div>
             <div className="col-12 mb-3">
-                <Input
+                <label>password</label>
+                <input
                     name="password"
-                    label="Password"
                     placeholder="Your password"
                     type="password"
-                    // value={form.password}
-                    // onChange={onChange}
-                    // hasError={errors.password && true}
-                    // error={errors.password}
                 />
             </div>
             <div className="col-12 mb-3">
-                <Input
+                <label>password Repeat</label>
+                <input
                     name="passwordRepeat"
-                    label="Password Repeat"
                     placeholder="Repeat your password"
                     type="password"
-                    // value={form.passwordRepeat}
-                    // onChange={onChange}
-                    // hasError={passwordRepeatError && true}
-                    // error={passwordRepeatError}
                 />
             </div>
             <div className="text-center">
-                <ButtonWithProgress
-                    // onClick={onClickSignup}
-                    // disabled={pendingApiCall || passwordRepeatError ? true : false}
-                    // pendingApiCall={pendingApiCall}
-                    text="Sign Up"
-                />
+                <button
+                    className="add-button"
+                    onClick={onClickSignup}>
+                    Add food
+                </button>
             </div>
         </div>
     );
 }
 
-UserSignupPage.defaultProps = {
-    actions: {
-        postSignup: () =>
-            new Promise((resolve, reject) => {
-                resolve({});
-            })
-    }
-};
-
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         actions: {
-//             postSignup: (user) => dispatch(authActions.signupHandler(user))
-//         }
-//     };
-// };
-
 export default UserSignupPage;
 
-// export default connect(null, mapDispatchToProps)(UserSignupPage);
