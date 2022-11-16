12546049f0409dbc98306c83dea00af4573da742
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-11-16 18:29:10 +0100

    Project Structure

--- src/frontend/src/redux/authActions.js
+++
@@ -1,8 +0,0 @@
-export const signupHandler = (user) => {
-    alert("singup")
-    // return function(dispatch) {
-    //     return apiCalls.signup(user).then((response) => {
-    //         return dispatch(loginHandler(user));
-    //     });
-    // };
-};
