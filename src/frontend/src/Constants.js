a1f0eb6db4d293e2f16dc92c54a712f7cd97f72f
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-12-01 21:13:16 +0100

    prod and dev constants

--- src/frontend/src/Constants.js
+++ src/frontend/src/Constants.js
@@ -1,4 +1,5 @@
-// Constants.js
-// export const API_URL = 'http://dietplannerrds-env.eba-s2nrkgap.eu-north-1.elasticbeanstalk.com/' // Production
-export const API_URL = 'http://localhost:5000/' // Development
-// Component.js
+const prod = 'http://dietplannerrds-env.eba-s2nrkgap.eu-north-1.elasticbeanstalk.com/';
+
+const dev = 'http://localhost:5000/';
+
+export const API_URL = process.env.NODE_ENV === 'development' ? dev : prod;
