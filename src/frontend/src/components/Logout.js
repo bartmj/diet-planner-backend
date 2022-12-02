6e634c2169e0cda6dda99fcedd0b9bab23aa56da
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-12-02 11:21:10 +0100

    logout works

---
+++ src/frontend/src/components/Logout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logout = () => {
+
+    return (
+        <div className="container">
+            <h3>You have been logged out!</h3>
+        </div>
+    )
+}
+
+export default Logout;
