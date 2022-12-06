4549429e894149d1940d5b0ae10dd0f602f0e83f
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-12-06 21:35:07 +0100

    MailConfig

--- src/main/java/com/example/dietplanner/user/security/mail/MailConfig.java
+++ src/main/java/com/example/dietplanner/user/security/mail/MailConfig.java
@@ -1,4 +1,31 @@
 package com.example.dietplanner.user.security.mail;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.cloud.aws.mail.simplemail.SimpleEmailServiceMailSender;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+
+@Configuration
 public class MailConfig {
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider("default"))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+
+    @Bean
+    public MailSender mailSender(
+            AmazonSimpleEmailService amazonSimpleEmailService) {
+        return new SimpleEmailServiceMailSender(amazonSimpleEmailService);
+    }
 }
+
+
